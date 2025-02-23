006c36537cf83decf6378edf05ba97152f0a7245
itsDikshaMahanot <jainmani786@gmail.com>
2025-02-23 17:00:43 +0530

    added components for food item to display with store context to handle use state.

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, SetCategory }) => {
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore Menu</h1>
@@ -11,8 +11,11 @@ const ExploreMenu = () => {
                 {
                     menu_list.map((item, index) => {
                         return (
-                            <div key={index} className='explore-menu-list-item'>
-                                <img src={item.menu_image} alt='menu' />
+                            <div onClick={() => {
+                                console.log('Selected Category:', item.menu_name);
+                                SetCategory(prev => prev === item.menu_name ? "All" : item.menu_name);
+                            }} key={index} className='explore-menu-list-item'>
+                                <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt='menu' />
                                 <p>{item.menu_name}</p>
                             </div>
                         )
